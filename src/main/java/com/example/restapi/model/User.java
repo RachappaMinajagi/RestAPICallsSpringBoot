89cf9ca2ed90c32c6885a33f3f471c3318ea57b9
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-04-20 23:10:15 +0530

    Display the messages in web browser message post name

---
+++ src/main/java/com/example/restapi/model/User.java
@@ -0,0 +1,31 @@
+package com.example.restapi.model;
+
+public class User {
+
+    /**
+     * variables
+     */
+    private String firstName;
+    private String lastName;
+
+    /**
+     * using gette and setter
+     *
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
